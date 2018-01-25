22367be94033b256e12967abf0a7044d56b045e5
Nisen <imnisen@gmail.com>
2018-01-25 23:30:16 +0800

    define some contant to easy the test case

--- t/cl-httpbin.lisp
+++ t/cl-httpbin.lisp
@@ -28,6 +28,22 @@
 (defvar +x-arg2+ "x-arg2")
 (defvar +x-v1+ "x-v1")
 (defvar +x-v2+ "x-v2")
+(defvar +content-k1+ "content-k1")
+(defvar +content-k2+ "content-k2")
+(defvar +content-v1+ "content-v1")
+(defvar +content-v2+ "content-v2")
+
+;; To finish
+;; (defmacro uri-to-url (uri &rest args)
+;;   (concatenate 'string
+
+;;                )
+;;   (let ((url (format nil "http://127.0.0.1~a" ,uri)))
+;;     (loop for x on args by #'cddr
+;;          (format "~a=~a")))
+
+
+;;   )
 
 
 
@@ -40,14 +56,14 @@
 
 (subtest "Testing /ip"
   (multiple-value-bind (body status)
-      (dex:get "http://127.0.0.1:5000/ip")
+      (dex:get (uri-to-url "/ip"))
     (is status +status-ok+)
     (is +origin+ (gethash "origin" (yason:parse body)))))
 
 
 (subtest "Testing /headers"
   (multiple-value-bind (body status)
-      (dex:get "http://127.0.0.1:5000/headers"
+      (dex:get (uri-to-url "/headers")
                :headers `(("User-Agent" . ,+user-agent+)
                           ("Accept" . "*/*")
                           (,+x-key1+ . ,+x-value1+)
@@ -66,7 +82,7 @@
 
 (subtest "Testing /user-agent"
   (multiple-value-bind (body status)
-      (dex:get "http://127.0.0.1:5000/user-agent"
+      (dex:get (uri-to-url  "/user-agent")
                :headers `(("User-Agent" . ,+user-agent+)))
     (is status +status-ok+)
     (let* ((body-json (yason:parse body)))
@@ -83,6 +99,31 @@
     (let* ((body-json (yason:parse body)))
       (is +origin+ (gethash "origin" body-json))
       (is (format nil "http://127.0.0.1:5000/get?~a=~a&~a=~a" +x-arg1+ +x-v1+ +x-arg2+ +x-v2+) (gethash "url" body-json))
+      (is +x-v1+ (gethash +x-arg1+ (gethash "args" body-json)))
+      (is +x-v2+ (gethash +x-arg2+ (gethash "args" body-json)))
+      (is +user-agent+ (gethash "user-agent" (gethash "headers" body-json)))
+      (is +accept+ (gethash "accept" (gethash "headers" body-json)))
+      (is +x-value1+ (gethash +x-key1+ (gethash "headers" body-json)))
+      (is +x-value2+ (gethash +x-key2+ (gethash "headers" body-json))))))
+
+
+(subtest "Testing /anything"
+  (multiple-value-bind (body status)
+      (dex:get "http://127.0.0.1:5000/anything")
+    (is status 200))
+  (multiple-value-bind (body status)
+      (dex:post (format nil "http://127.0.0.1:5000/anything?~a=~a&~a=~a" +x-arg1+ +x-v1+ +x-arg2+ +x-v2+)
+                :headers `(("User-Agent" . ,+user-agent+)
+                           ("Accept" . "*/*")
+                           (,+x-key1+ . ,+x-value1+)
+                           (,+x-key2+ . ,+x-value2+))
+                :content `((,+content-k1+ . ,+content-v1+)
+                           (,+content-k2+ . ,+content-v2+)))
+    (format t "~a" body)
+    (is status 200)
+    (let* ((body-json (yason:parse body)))
+      (is +origin+ (gethash "origin" body-json))
+      (is (format nil "http://127.0.0.1:5000/anything?~a=~a&~a=~a" +x-arg1+ +x-v1+ +x-arg2+ +x-v2+) (gethash "url" body-json))
       (is +x-v1+ (gethash +x-arg1+ (gethash "args" body-json)))
       (is +x-v2+ (gethash +x-arg2+ (gethash "args" body-json)))
       (is +user-agent+ (gethash "user-agent" (gethash "headers" body-json)))
